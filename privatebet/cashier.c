16d77604086e30e0a421956ba724e2345e3a1255
satindergrewal <grewal@lavabit.com>
2020-12-22 22:12:58 +1300

    debug

--- privatebet/cashier.c
+++ privatebet/cashier.c
@@ -133,8 +133,8 @@ void bet_check_cashiers_status()
 			fprintf(stderr, "Failed to print monitor.\n");
 		}
 		printf("temp_string - bet_check_cashiers_status() - %s\n", temp_string);
-		int tmpcompare = jstr(temp, "live");
-		printf("tmpcompare - %d\n", tmpcompare);
+		// int tmpcompare = jstr(temp, "live");
+		printf("tmpcompare - %d\n", jstr(temp, "live"));
 
 		if ((temp) && (jstr(temp, "live") == 0)) {
 			notary_status[i] = 1;
