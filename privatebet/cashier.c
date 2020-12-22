15fd08a6afde0e769f5605dcfaeb507ae8fa812f
satindergrewal <grewal@lavabit.com>
2020-12-23 00:33:05 +1300

    debug prints for bet_tcp_sock_address

--- privatebet/cashier.c
+++ privatebet/cashier.c
@@ -856,9 +856,11 @@ cJSON *bet_msg_cashier_with_response_id(cJSON *argjson, char *cashier_ip, char *
 	memset(bind_sub_addr, 0x00, sizeof(bind_sub_addr));
 	memset(bind_push_addr, 0x00, sizeof(bind_push_addr));
 
+	printf("bet_tcp_sock_address 1 - %s\n", bet_tcp_sock_address(0, bind_sub_addr, cashier_ip, cashier_pubsub_port));
 	bet_tcp_sock_address(0, bind_sub_addr, cashier_ip, cashier_pubsub_port);
 	c_subsock = bet_nanosock(0, bind_sub_addr, NN_SUB);
 
+	printf("bet_tcp_sock_address 2 - %s\n", bet_tcp_sock_address(0, bind_push_addr, cashier_ip, cashier_pushpull_port));
 	bet_tcp_sock_address(0, bind_push_addr, cashier_ip, cashier_pushpull_port);
 	c_pushsock = bet_nanosock(0, bind_push_addr, NN_PUSH);
 
