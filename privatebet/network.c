9e0834c4970f7e811c871d64e28dcfaa806c397b
satindergrewal <grewal@lavabit.com>
2021-02-04 21:33:03 +1300

    nn_connect debug print enabled

--- privatebet/network.c
+++ privatebet/network.c
@@ -67,9 +67,8 @@ int32_t bet_nanosock(int32_t bindflag, char *endpoint, int32_t nntype)
 				printf("connect to %s error for %s\n", endpoint, nn_strerror(nn_errno()));
 				nn_close(sock);
 				return (-1);
-			} /*else
-				printf("nntype.%d connect to %s connectsock.%d\n", nntype, endpoint, sock);
-				*/
+			} else
+				printf("nn_connect - nntype.%d connect to %s connectsock.%d\n", nntype, endpoint, sock);
 		} else {
 			if (nn_bind(sock, endpoint) < 0) {
 				printf("bind to %s error for %s\n", endpoint, nn_strerror(nn_errno()));
